68778952999a736e447c78435149f0ee0d47531c
Akagi201 <akagi201@gmail.com>
2021-11-22 15:57:01 +0800

    feat(sshpass): update version

--- sshpass.rb
+++ sshpass.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Sshpass < Formula
   desc 'The sshpass utility helps administrators more easily manage SSH connections in scripts.'
-  version 1.09
+  version '1.09.1'
   url 'https://github.com/dora38/sshpass.git', :branch => 'main', :using => :git
   homepage 'http://sourceforge.net/projects/sshpass'
   license 'MIT'
